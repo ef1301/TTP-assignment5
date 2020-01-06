482057c44a7b9ef6b09f404d0f3e0a6bc9d8e041
Sami Beig <samibeig@Samis-MacBook-Pro.local>
2020-01-06 18:27:56 -0500

    added addRow() and addCol()

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var rows = 0;
 var columns = 0;
+var mainGrid = document.getElementById("main-grid");
 
 //References the div that holds the grid
 var grid = document.getElementsByTagName("table")[0];
@@ -11,4 +12,30 @@ function deleteRow(){
 
 function deleteColumn(){
 
+}
+
+function addRow(){
+  let newRow = document.createElement("tableRow");
+
+  //Populate the row with squares
+  for (let i = 0; i < columns; i++){
+    let cell = document.createElement("tableCol");
+    newRow.appendChild(cell);
+    console.log("added row");
+  }
+  mainGrid.appendChild(newRow);
+  rows++;
+}
+
+function addCol(){
+  let newCol = document.createElement("tableCol");
+
+  //Populate the col with squares
+  for(let i = 0; i < rows; i++){
+    let cell = document.createElement("tableRow");
+    newCol.appendChild(cell);
+    console.log("added col");
+  }
+  mainGrid.appendChild(newCol);
+  columns++;
 }
