23689d00d78a53e0375fdf9e9edc5042dc8d9d07
Emily Fang <fangemily60@gmail.com>
2020-01-06 18:06:11 -0500

    div grid changed to table

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var rows = 0;
 var columns = 0;
 
 //References the div that holds the grid
-var grid = document.getElementsByClassName("grid")[0];
-//console.log(grid);
+var grid = document.getElementsByTagName("table")[0];
+console.log(grid);
 
 function deleteRow(){
-    
 
 }
 
