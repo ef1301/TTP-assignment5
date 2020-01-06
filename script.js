f847f3827037f1917d955db73530a4ac3fff68dc
Emily Fang <fangemily60@gmail.com>
2020-01-06 17:50:51 -0500

    referenced grid

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var rows = 0;
 var columns = 0;
 
+//References the div that holds the grid
+var grid = document.getElementsByClassName("grid")[0];
+//console.log(grid);
+
 function deleteRow(){
+    
 
 }
 
