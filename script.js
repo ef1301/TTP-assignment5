7be6fda3626e8621143625c45d30deb1d9741211
Emily Fang <fangemily60@gmail.com>
2020-01-06 18:23:33 -0500

    All remove and clear functions made but not tested

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ var rows = 0;
 var columns = 0;
 
 //References the div that holds the grid
-var grid = document.getElementsByClassName("grid")[0];
+var grid = document.getElementsByTagName("table")[0];
 //console.log(grid);
 
 function deleteRow(){
-    
-
+    if(rows > 0){
+	grid.deleteRow(0);
+    }
+    alert("You don't have any more rows to remove!");
 }
 
 function deleteColumn(){
-
+    let rows = document.getElementsByTagName("tr");
+    for(let i = 0; i < rows.length; i++){
+	rows[i][rows.length - 1].remove;
+    }
 }
+
+function clearAll(){
+    let rows = document.getElementsByTagName("tr");
+    for(let i = 0; i < rows.length; i++){
+	rows[i].remove;
+    }
+}
+
+document.getElementById("removeRow").addEventListener(onclick, deleteRow);
+document.getElementById("removeColumn").addEventListener(onclick, deleteColumn);
+document.getElementById("clear").addEventListener(onclick, clearAll);
