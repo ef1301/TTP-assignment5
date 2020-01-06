63b2f14f588f7488fa7e64afcf7a5dc312492374
Emily Fang <fangemily60@gmail.com>
2020-01-06 18:32:10 -0500

    clearAll function improved

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function deleteColumn(){
 function clearAll(){
     let rows = document.getElementsByTagName("tr");
     for(let i = 0; i < rows.length; i++){
-	rows[i].remove;
+	grid.deleteRow(i);
     }
 }
 
