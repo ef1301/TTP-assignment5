6e3a0511304fa68e30a4f9448660c723e0e3a4ca
Sami Beig <samibeig@Samis-MacBook-Pro.local>
2020-01-06 20:03:45 -0500

    attempting to fix addRow() and addCol()

--- script.js
+++ script.js
@@ -4,38 +4,67 @@ var mainGrid = document.getElementById("main-grid");
 
 //References the div that holds the grid
 var grid = document.getElementsByTagName("table")[0];
-console.log(grid);
-
-function deleteRow(){
-
-}
-
-function deleteColumn(){
-
-}
 
 function addRow(){
-  let newRow = document.createElement("tableRow");
+  rows += 1;
+  let newRow = document.createElement("tr");
 
   //Populate the row with squares
   for (let i = 0; i < columns; i++){
-    let cell = document.createElement("tableCol");
+    let cell = document.createElement("td");
     newRow.appendChild(cell);
     console.log("added row");
   }
-  mainGrid.appendChild(newRow);
-  rows++;
+  grid.appendChild(newRow);
 }
 
+
 function addCol(){
-  let newCol = document.createElement("tableCol");
+  columns += 1;
+  let newCol = document.createElement("td");
 
   //Populate the col with squares
   for(let i = 0; i < rows; i++){
-    let cell = document.createElement("tableRow");
+    let cell = document.createElement("tr");
     newCol.appendChild(cell);
     console.log("added col");
   }
-  mainGrid.appendChild(newCol);
-  columns++;
+  grid.appendChild(newCol);
 }
+
+
+function deleteRow(){
+  if(rows > 0){
+    grid.deleteRow(0);
+    rows--;
+  }
+  alert("You don't have any more rows to remove!");
+}
+
+function deleteColumn(){
+    let rows = document.getElementsByTagName("tr");
+    if(columns > 0){
+	for(let i = 0; i < rows.length; i++){
+	    rows[i][rows.length - 1].removeCell();
+	}
+	columns -= 1;
+    }
+    alert("You don't have any more columns to remove!");
+    
+}
+
+function clearAll(){
+    let rows = document.getElementsByTagName("tr");
+    for(let i = 0; i < rows.length; i++){
+	grid.deleteRow(i);
+    }
+    rows = 0;
+    columns = 0;
+}
+
+document.getElementById("addRow").addEventListener(onclick, addRow);
+document.getElementById("addColumn").addEventListener(onclick, addCol);
+
+document.getElementById("removeRow").addEventListener(onclick, deleteRow);
+document.getElementById("removeColumn").addEventListener(onclick, deleteColumn);
+document.getElementById("clear").addEventListener(onclick, clearAll);
