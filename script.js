5da1c8e340909a3cf4b5a7b99a36c14bb56d22ab
Sami Beig <samibeig@Samis-MacBook-Pro.local>
2020-01-06 22:56:29 -0500

    changed dropdown for easier access to colors

--- script.js
+++ script.js
@@ -1,65 +1,90 @@
 var rows = 0;
 var columns = 0;
 var mainGrid = document.getElementById("main-grid");
+var currentColor = document.getElementById("myColor");
 
-//References the div that holds the grid
+// References the div that holds the grid
 var grid = document.getElementsByTagName("table")[0];
 
 function addRow(){
-  rows += 1;
-  let newRow = document.createElement("tr");
-
-  //Populate the row with squares
-  for (let i = 0; i < columns; i++){
-    let cell = document.createElement("td");
-    newRow.appendChild(cell);
+    let newRow = document.createElement("tr");
+    
+    // edge case: the table is empty
+    if (columns == 0) columns = 1;
+    
+    // Populate the row with squares
+    for (let i = 0; i < columns; i++){
+	let cell = document.createElement("td");
+	newRow.appendChild(cell);
+    }
+    grid.appendChild(newRow);
     console.log("added row");
-  }
-  grid.appendChild(newRow);
+    rows += 1;
 }
 
 
 function addCol(){
-  columns += 1;
-  let newCol = document.createElement("tr");
+    let tableRows = document.getElementsByTagName("tr");
 
-  //Populate the col with squares
-  for(let i = 0; i < rows; i++){
-    let cell = document.createElement("td");
-    newCol.appendChild(cell);
+    // edge case: there are no rows to add a cell to
+    if (rows == 0) {
+	    grid.appendChild(document.createElement("tr"));
+	    rows++;
+    }
+    
+    // Populate the col with squares
+    for(let i = 0; i < rows; i++){
+	let cell = document.createElement("td");
+	tableRows[i].appendChild(cell);
+    }
     console.log("added col");
-  }
-  grid.appendChild(newCol);
+    columns += 1;
 }
 
 
 function deleteRow(){
-  if(rows > 0){
-    grid.deleteRow(0);
-    rows--;
-  }
-  alert("You don't have any more rows to remove!");
+    if(rows > 0){
+	grid.deleteRow(0);
+	rows--;
+	if(rows == 0){
+	    columns = 0;
+	}
+    }
+    else{
+	alert("You don't have any more rows to remove!");
+    }
 }
 
 function deleteColumn(){
     let rows = document.getElementsByTagName("tr");
     if(columns > 0){
 	for(let i = 0; i < rows.length; i++){
-	    rows[i][rows.length - 1].removeCell();
+	    rows[i].deleteCell(0);
 	}
-	columns -= 1;
+	columns--;
+	if(columns == 0){
+	    rows = 0;
+	}
     }
-    alert("You don't have any more columns to remove!");
-    
+    else{
+	alert("You don't have any more columns to remove!");
+    }
 }
 
 function clearAll(){
-    let rows = document.getElementsByTagName("tr");
-    for(let i = 0; i < rows.length; i++){
-	grid.deleteRow(i);
+    for(let i = 0; i < rows; i++){
+	grid.deleteRow(0);
     }
     rows = 0;
     columns = 0;
+}
+
+function fillAll(){
+  //let parent = document.getElementsByTagName("table");
+  //var arr = Array.prototype.slice.call( document.getElementsByTagName("table") )
+  console.log(Array.prototype.slice.call( document.getElementsByTagName("table") ));
+
+
 }
 
 document.getElementById("addRow").addEventListener(onclick, addRow);
@@ -68,3 +93,9 @@ document.getElementById("addColumn").addEventListener(onclick, addCol);
 document.getElementById("removeRow").addEventListener(onclick, deleteRow);
 document.getElementById("removeColumn").addEventListener(onclick, deleteColumn);
 document.getElementById("clear").addEventListener(onclick, clearAll);
+
+document.getElementById("fillAll").addEventListener(onclick, fillAll);
+
+//console.log(Array.prototype.slice.call( document.getElementsByTagName("table") ));
+
+fillAll();
